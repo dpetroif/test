08f1444d25fd554e64bb727792192775273eec94
dpetroif <petro_if@e-mail.ua>
2013-03-28 11:51:18 +0200

    1first one

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+	<head> 
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+		<title>CAIH</title>
+		<link type="text/css" rel="stylesheet" href="style.css">
+	</head>
+	<body>
+		<div id="rightp">
+			<a href="#"><div  style="top:15px; left:8px;">
+				<img src="images/1.jpg" alt="" style="width:160px; height:120px;">
+				<p>galereya do portfolio</p>
+			</div></a>
+			<a href="#"><div  style="top:15px; left:193px;">
+				<img src="images/1.jpg" alt="" style="width:160px; height:120px; ">
+				<p>komentar</p>
+			</div></a>
+			<a href="#"><div style="top:15px; left:378px;">
+				<img src="images/1.jpg" alt="" style="width:160px; height:120px;">
+				<p>komentar</p>
+			</div></a>
+			<a href="#"><div  style="top:15px; left:563px;">
+				<img src="images/1.jpg" alt="" style="width:160px; height:120px;">
+				<p>komentar</p>
+			</div></a>
+			<a href="#"><div  style="top:195px; left:8px;">
+				<img src="images/1.jpg" alt="" style="width:160px; height:120px;">
+				<p>komentar</p>
+			</div></a>
+			<a href="#"><div  style="top:195px; left:193px;">
+				<img src="images/1.jpg" alt="" style="width:160px; height:120px;">
+				<p>komentar</p>
+			</div></a>
+			<a href="#"><div  style="top:195px; left:378px;">
+				<img src="images/1.jpg" alt="" style="width:160px; height:120px;">
+				<p>komentar</p>
+			</div></a>
+			<a href="#"><div  style="top:195px; left:563px;">
+				<img src="images/1.jpg" alt="" style="width:160px; height:120px;">
+				<p>komentar</p>
+			</div></a>
+			<a href="#"><div  style="top:375px; left:8px;">
+				<img src="images/1.jpg" alt="" style="width:160px; height:120px;">
+				<p>komentar</p>
+			</div></a>
+			<a href="#"><div style="top:375px; left:193px;">
+				<img src="images/1.jpg" alt="" style="width:160px; height:120px;">
+				<p>komentar</p>
+			</div></a>
+			<a href="#"><div  style="top:375px; left:378px;">
+				<img src="images/1.jpg" alt="" style="width:160px; height:120px;">
+				<p>komentar</p>
+			</div></a>
+			<a href="#"><div  style="top:375px; left:563px;">
+				<img src="images/1.jpg" alt="" style="width:160px; height:120px;">
+				<p>komentar</p>
+			</div></a>
+			
+		</div>
+	</body>
+</html>
